66b2b240ad04def7d20df3d33251413f1964f784
Mohammad Sazzad Hossain <72731635+SSazzad@users.noreply.github.com>
2021-04-15 06:01:21 -0800

    Add files via upload
    autogui(Python Code) Uploaded

---
+++ autogui.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+while True:
+    time.sleep(4)
+    pyautogui.typewrite('In some ways, programming is like painting..')
+    time.sleep(2)
+    pyautogui.press('Enter')
+ In some ways, programming is like
+if painting.:
+
+
